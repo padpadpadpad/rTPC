e36e1c2d1cedaba65aab94067265ca9e4fc4ed91
Daniel Padfield <d.padfieldscfc@gmail.com>
2019-02-28 16:31:30 +0000

    Define get_rmax function

---
+++ R/get_rmax.R
@@ -0,0 +1,31 @@
+#' Estimate maximum rate
+#'
+#' @description Estimates maximum rate from a model of a thermal performance curve.
+#' @param model nls model object that contains a model of a thermal performance curve
+#' @author Daniel Padfield
+#' @details Maximum rate is calculated by predicting over the temperature range using the previously estimated parameters and picking the maximum rate value. Predictions are done every 0.001 temperature value.
+#'
+#' @export get_rmax
+
+get_rmax <- function(model){
+
+  # capture environment from model - contains data
+  x <- model$m$getEnv()
+
+  # get the name of the temperature column
+  formula <- stats::as.formula(model$m$formula())
+  param_ind <- all.vars(formula[[3]])[! all.vars(formula[[3]]) %in%
+                                        names(model$m$getPars())]
+
+  # extract the temperature values
+  vals <- x[[param_ind]]
+  newdata <- data.frame(x = seq(min(vals), max(vals), by = 0.001), stringsAsFactors = FALSE)
+  # rename to be the correct column name
+  names(newdata) <- param_ind
+
+  # predict over a whole wide range of data
+  newdata$preds <- stats::predict(model, newdata = newdata)
+  rmax = newdata[newdata$preds == max(newdata$preds),preds]
+  return(rmax)
+}
+
