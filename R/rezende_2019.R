57346bb322e003c2d88bc5eb3580fc1d67d77847
Daniel Padfield <d.padfieldscfc@gmail.com>
2019-08-12 14:44:34 +0100

    define rezende_2019

---
+++ R/rezende_2019.R
@@ -0,0 +1,15 @@
+#' Rezende 2019 model
+#'
+#' @param temp temperature in degrees centigrade
+#' @param a parameter describing shifts in the vertical axis
+#' @param b parameter threshold temperature beyond which the downward curve starts
+#' @param c parameter controlling the rate of decline beyond the threshold temperature, b.
+#' @author Daniel Padfield
+#' @references Rezende, Enrico L., and Francisco Bozinovic. "Thermal performance across levels of biological organization." Philosophical Transactions of the Royal Society B 374.1778 (2019): 20180549.
+#' @export rezende_2019
+
+rezende_2019 <- function(temp, q10, a, b, c){
+
+  est <-{ifelse(temp < b, (a*10^(log10(q10)/(10/temp))), (a*10^(log10(q10)/(10/temp)))*(1-c*(b - temp)^2))}
+  return(est)
+}
