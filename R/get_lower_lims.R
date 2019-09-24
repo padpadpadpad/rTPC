e90552a6075334e377fbe157e14b5bbc496af0d4
Daniel Padfield <d.padfieldscfc@gmail.com>
2019-09-24 15:20:13 +0100

    Add error message if model_name is specified wrongly

--- R/get_lower_lims.R
+++ R/get_lower_lims.R
@@ -10,6 +10,13 @@
 
 get_lower_lims <- function(x, y, model_name) {
 
+  # error message for wrong spelling of model name
+  mod_names <- c('sharpeschoolhigh_1981', 'sharpeschoollow_1981', 'sharpeschoolfull_1981', 'johnsonlewin_1946', 'lactin2_1995', 'oneill_1972', 'quadratic_2008', 'ratkowsky_1983', 'rezende_2019', 'spain_1982', 'thomas_2012', 'thomas_2017', 'weibull_1995', 'kamykowski_1985', 'joehnk_2008', 'hinshelwood_1947', 'gaussian_1987', 'flinn_1991', 'delong_2017', 'briere2_1999', 'boatman_2017')
+
+  if (model_name %in% mod_names == FALSE){
+    stop("supplied model_name is not an available model in rTPC. Please check the spelling of model_name.")
+  }
+
   # make data frame
   d <- data.frame(x, y, stringsAsFactors = FALSE)
   d <- d[order(d$x),]
