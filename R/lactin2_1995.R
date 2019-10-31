2b6a466bb80b2e191b9b69ec6754a024c1bfc1e8
Hannah O'Sullivan <hjosullivan123@gmail.com>
2019-10-31 16:32:31 +0000

    Add documentation for lactin2_1995

--- R/lactin2_1995.R
+++ R/lactin2_1995.R
@@ -7,7 +7,21 @@
 #' @param delta_t thermal safety margin
 #' @author Daniel Padfield
 #' @references Lactin, D.J., Holliday, N.J., Johnson, D.L. & Craigen, R. Improved rate models of temperature-dependent development by arthropods. Environ. Entomol. 24, 69-75 (1995)
+#' @examples
+#' # load in data
+#' data('chlorella_tpc')
+#' d <- subset(chlorella_tpc, curve_id == 1)
 #'
+#' # get start values and fit model
+#' start_vals <- get_start_vals(d$temp, d$rate, model_name = 'lactin2_1995')
+#' mod <- minpack.lm::nlsLM(rate~lactin2_1995(temp = temp, a, b, tmax, delta_t),
+#' data = d,
+#' start = start_vals,
+#' control = minpack.lm::nls.lm.control(maxiter = 100))
+#'
+#' # look at model
+#' summary(mod)
+#' est_params(mod)
 #' @export lactin2_1995
 
 lactin2_1995 <- function(temp, a, b, tmax, delta_t){
