673f981a3e0c0e50834c103687395697112f0e40
Daniel Padfield <d.padfieldscfc@gmail.com>
2020-06-03 09:37:58 +0100

    Update beta_2012 model

--- R/beta_2012.R
+++ R/beta_2012.R
@@ -8,6 +8,15 @@
 #' @param e dimensionless parameter
 #' @author Daniel Padfield
 #' @references Niehaus, Amanda C., et al. Predicting the physiological performance of ectotherms in fluctuating thermal environments. Journal of Experimental Biology 215.4: 694-701 (2012)
+#' @details Equation:
+#' \deqn{rate=\frac{a\left(\frac{temp-b+\frac{c(d-1)}{d+e-2}}{c}\right)^{d-1}  \cdot \left(1-\frac{temp-b+\frac{c(d-1)}{d+e-2}}{c}\right)^{e-1}}{{\left(\frac{d-1}{d+e-2}\right)}^{d-1}\cdot \left(\frac{e-1}{d+e-2}\right)^{e-1}}}{%
+#' rate = (a.((temp - b + ((c.(d-1))/(d + e - 2)))/c)^(d-1).(1 - ((temp - b + ((c.(d-1))/(d + e - 2)))/c))^(e-1)) / (((d-1)/(d + e - 2))^(d-1).((e-1)/(d + e - 2))^(e-1))}
+#'
+#' Start values in \code{get_start_vals} are derived from the data or sensible values from the literature.
+#'
+#' Limits in \code{get_lower_lims} and \code{get_upper_lims} are derived from the data or based extreme values that are unlikely to occur in ecological settings.
+#'
+#' @note Generally we found this model difficult to fit.
 #' @examples
 #' \dontrun{
 #' #' data('chlorella_tpc')
@@ -26,6 +35,13 @@
 #' @export beta_2012
 
 beta_2012 <- function(temp, a, b, c, d, e){
-  est <- (a*((temp - b + ((c*(d-1))/(d + e - 2)))/c)^(d-1) * (1 - ((temp - b + ((c*(d-1))/(d + e - 2)))/c))^(e-1)) / (((d-1)/(d + e - 2))^(d-1) * ((e-1)/(d + e - 2))^(e-1))
-  return(est)
+  part1 <- a*((temp - b + ((c*(d-1))/(d + e - 2)))/c)^(d-1)
+  part2 <- (1 - ((temp - b + ((c*(d-1))/(d + e - 2)))/c))^(e-1)
+  part3 <- ((d-1)/(d + e - 2))^(d-1)
+  part4 <- ((e-1)/(d + e - 2))^(e-1)
+
+  temp <- part1 * part2
+  temp2 <- part3 * part4
+  return(temp/temp2)
+
 }
--- R/sharpeschoolfull_1981.R
+++ R/sharpeschoolfull_1981.R
@@ -20,6 +20,7 @@
 #' Start values in \code{get_start_vals} are derived from the data.
 #'
 #' Limits in \code{get_lower_lims} and \code{get_upper_lims} are derived from the data or based  extreme values that are unlikely to occur in ecological settings.
+#' @note Generally we found this model easy to fit.
 #' @examples
 #' # load in data
 #' data('chlorella_tpc')
