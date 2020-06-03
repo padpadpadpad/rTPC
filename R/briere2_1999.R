ed4bc978768334c16b66ef1d9005883c67a83115
Daniel Padfield <d.padfieldscfc@gmail.com>
2020-06-03 09:55:52 +0100

    Update briere2_1999 model

--- R/briere2_1999.R
+++ R/briere2_1999.R
@@ -1,16 +1,19 @@
-#' Briere2 et al. model for fitting thermal performance curves
+#' Briere2 model for fitting thermal performance curves
 #'
 #' @param temp temperature in degrees centigrade
 #' @param tmin low temperature (ºC) at which rates become negative
 #' @param tmax high temperature (ºC) at which rates become negative
 #' @param a scale parameter to adjust maximum rate of the curve
 #' @param b shape parameter to adjust the asymmetry of the curve
-#' @author Daniel Padfield
 #' @references Brière, J.F., Pracros, P., Le Roux, A.Y., Pierre, J.S.,  A novel rate model of temperature-dependent development for arthropods. Environmental Entomololgy, 28, 22–29 (1999)
 #' @details Equation:
 #' \deqn{rate=a\cdot temp \cdot(temp - t_{min}) \cdot (t_{max} - T)^{\frac{1}{b}}}{%
 #' rate = a.temp.(temp - tmin).(tmax - temp)^(1/b)}
 #'
+#' Start values in \code{get_start_vals} are derived from the data or sensible values from the literature.
+#'
+#' Limits in \code{get_lower_lims} and \code{get_upper_lims} are derived from the data or based extreme values that are unlikely to occur in ecological settings.
+#' @note Generally we found this model easy to fit.
 #' @examples
 #' # load in data
 #' data('chlorella_tpc')
