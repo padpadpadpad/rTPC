5e98f02dcc89cd77b4a848fe0959cb33828dbb0e
Daniel Padfield <d.padfieldscfc@gmail.com>
2020-06-03 14:45:26 +0100

    Update flinn_1991 model

--- R/delong_2017.R
+++ R/delong_2017.R
@@ -6,7 +6,7 @@
 #' @param ef temperature dependence of folding the enzymes used in the metabolic reaction, relative to the melting temperature (eV)
 #' @param tm melting temperature in degrees centigrade
 #' @param ehc temperature dependence of the heat capacity between the folded and unfolded state of the enzymes, relative to the melting temperature (eV)
-#' @references DeLong, John P., et al. "The combined effects of reactant kinetics and enzyme stability explain the temperature dependence of metabolic rates." Ecology and evolution 7.11 (2017): 3940-3950.
+#' @references DeLong, John P., et al. The combined effects of reactant kinetics and enzyme stability explain the temperature dependence of metabolic rates. Ecology and evolution 7.11 (2017): 3940-3950.
 #' @details Equation:
 #' \deqn{rate = c \cdot exp\frac{-(e_b-(e_f(1-\frac{temp - 273.15}{t_m})+e_{hc} \cdot ((temp - 273.15) - t_m - (temp - 273.15) \cdot ln(\frac{temp - 273.15}{t_m}))))}{k \cdot (temp - 273.15)}}{%
 #' rate = c.exp(-(eb-(ef.(1-((temp + 273.15)/tm))+ehc.((temp + 273.15)-tm-((temp + 273.15).log((temp + 273.15)/tm)))))/(k.(temp + 273.15)))}
--- R/flinn_1991.R
+++ R/flinn_1991.R
@@ -4,11 +4,17 @@
 #' @param a parameter that controls the height of the curve
 #' @param b parameter that controls the slope of the initial increase of the curve
 #' @param c parameter that controls the position and steepness of the decline of the curve
-#' @author Daniel Padfield
 #' @references Flinn PW Temperature-dependent functional response of the parasitoid Cephalonomia waterstoni (Gahan) (Hymenoptera, Bethylidae) attacking rusty grain beetle larvae (Coleoptera, Cucujidae). Environmental Entomology, 20, 872–876, (1991)
 #' @details Equation:
 #' \deqn{rate= \frac{1}{1+a+b \cdot temp+c \cdot temp^2}}{%
 #' rate = 1 / (1 + a + b.temp + c.temp^2)}
+#'
+#' Start values in \code{get_start_vals} are derived from previous methods from the literature.
+#'
+#' Limits in \code{get_lower_lims} and \code{get_upper_lims} are based on extreme values that are unlikely to occur in ecological settings.
+#'
+#' @note Generally we found this model easy to fit.
+#'
 #' @examples
 #' \dontrun{
 #' # load in data
@@ -27,7 +33,7 @@
 #' est_params(mod)}
 #' @export flinn_1991
 
-flinn_1991 <- function(temp, a, b, c) {
+flinn_1991 <- function(temp, a, b, c){
   est <- 1/(1 + a + b * temp + c * temp^2)
   return(est)
 }
