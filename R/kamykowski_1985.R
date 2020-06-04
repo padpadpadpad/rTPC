d03ae8dc78cd1a7dcbc8ec1f375f2175c944ddfd
Daniel Padfield <d.padfieldscfc@gmail.com>
2020-06-04 11:51:43 +0100

    update kamykowski documentation

--- R/kamykowski_1985.R
+++ R/kamykowski_1985.R
@@ -1,13 +1,22 @@
-#' Kamykowski 1985 function
+#' Kamykowski model for fitting thermal performance curves
 #'
 #' @param temp temperature in degrees centigrade
-#' @param tmin low temperature at which rates become negative
-#' @param tmax high temperature at which rates become negative
+#' @param tmin low temperature (ºC) at which rates become negative
+#' @param tmax high temperature (ºC) at which rates become negative
 #' @param a parameter with no biological meaning
 #' @param b parameter with no biological meaning
 #' @param c parameter with no biological meaning
-#' @author Daniel Padfield
-#' @references Kamykowski, Daniel. "A survey of protozoan laboratory temperature studies applied to marine dinoflagellate behavior from a field perspective." Contributions in Marine Science. 1985. (1985).
+#' @details Equation:
+#' \deqn{rate= a \cdot \big[ 1 - exp^{-b\cdot \big(temp-t_{min}\big)}\big] \cdot \big[ 1-exp^{-c \cdot \big(t_{max}-temp\big)}\big]}{%
+#' rate =  a.(1 - exp(-b.(temp - tmin))).(1 - exp(-c.(tmax - temp)))}
+#'
+#' Start values in \code{get_start_vals} are derived from the data or sensible values from the literature.
+#'
+#' Limits in \code{get_lower_lims} and \code{get_upper_lims} are derived from the data or based extreme values that are unlikely to occur in ecological settings.
+#'
+#' @note Generally we found this model easy to fit.
+#'
+#' @references Kamykowski, Daniel. A survey of protozoan laboratory temperature studies applied to marine dinoflagellate behaviour from a field perspective. Contributions in Marine Science. (1985).
 #' @examples
 #' # load in data
 #' data('chlorella_tpc')
