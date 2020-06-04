84f1bc333ef1a47ad6bdd76a4533ce7535e06807
Daniel Padfield <d.padfieldscfc@gmail.com>
2020-06-04 11:31:43 +0100

    create functions for calculating safety margin and thermal tolerance

---
+++ R/get_thermalsafetymargin.R
@@ -0,0 +1,15 @@
+#' Estimate thermal safety margin of a thermal performance curve
+#'
+#' @param model nls model object that contains a model of a thermal performance curve
+#' @details Thermal safety margin is calculated as: CTmax - Topt. This is calculated using the functions \code{get_ctmax} and \code{get_topt}.
+#' @return Thermal safety margin (in ºC)
+#'
+#' @export get_thermalsafetymargin
+
+get_thermalsafetymargin <- function(model){
+
+  ctmax <- rTPC::get_ctmax(model)
+  topt <- rTPC::get_topt(model)
+  return(ctmax - topt)
+}
+
---
+++ R/get_thermaltolerance.R
@@ -0,0 +1,15 @@
+#' Estimate thermal tolerance of a thermal performance curve
+#'
+#' @param model nls model object that contains a model of a thermal performance curve
+#' @details Thermal tolerance is calculated as: CTmax - CTmin. This is calculated using the functions \code{get_ctmax} and \code{get_ctmin}.
+#' @return Thermal tolerance (in ºC)
+#'
+#' @export get_thermaltolerance
+
+get_thermaltolerance <- function(model){
+
+  ctmax <- rTPC::get_ctmax(model)
+  ctmin <- rTPC::get_ctmin(model)
+  return(ctmax - ctmin)
+}
+
