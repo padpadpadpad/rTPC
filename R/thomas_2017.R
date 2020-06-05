a1b81fc09df9084f6184c20cf800a4e805949a8a
Hannah O'Sullivan <hjosullivan123@gmail.com>
2020-06-05 11:45:58 +0100

    Update documentation

--- R/thomas_2017.R
+++ R/thomas_2017.R
@@ -1,4 +1,4 @@
-#' Thomas 2017
+#' Thomas model (2017)
 #'
 #' @param temp temperature in degrees centigrade
 #' @param a birth rate at 0 ºC
@@ -6,8 +6,15 @@
 #' @param c temperature-independent mortality term
 #' @param d along with e controls the exponential increase in mortality rates with temperature
 #' @param e along with d controls the exponential increase in mortality rates with temperature
-#' @author Daniel Padfield
-#' @references Thomas, Mridul K., et al. "Temperature–nutrient interactions exacerbate sensitivity to warming in phytoplankton." Global change biology 23.8 (2017): 3269-3280.
+#' @references Thomas, Mridul K., et al. Temperature–nutrient interactions exacerbate sensitivity to warming in phytoplankton. Global change biology 23.8 (2017): 3269-3280.
+#' @details Equation:
+#' \deqn{rate = a \cdot exp^{b \cdot temp} - (c + d \cdot exp^{e \cdot temp})}{%
+#' rate = a . exp(b . temp) - (c + d.(exp(e.temp)))}
+#'
+#' Start values in \code{get_start_vals} are derived from the data.
+#'
+#' Limits in \code{get_lower_lims} and \code{get_upper_lims} are derived from the data or based on extreme values that are unlikely to occur in ecological settings.
+#' @note Generally we found this model easy to fit.
 #' @examples
 #' # load in data
 #' data('chlorella_tpc')
