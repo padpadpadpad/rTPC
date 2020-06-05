043c5ad8fd138a655f9c54159db65445355af692
Daniel Padfield <d.padfieldscfc@gmail.com>
2020-06-05 12:25:54 +0100

    remove molnar_2013

--- R/get_start_vals.R
+++ R/get_start_vals.R
@@ -10,7 +10,7 @@
 
 get_start_vals <- function(x, y, model_name) {
 
-  mod_names <- c('sharpeschoolhigh_1981', 'sharpeschoollow_1981', 'sharpeschoolfull_1981', 'johnsonlewin_1946', 'lactin2_1995', 'oneill_1972', 'quadratic_2008', 'ratkowsky_1983', 'rezende_2019', 'spain_1982', 'thomas_2012', 'thomas_2017', 'weibull_1995', 'kamykowski_1985', 'joehnk_2008', 'hinshelwood_1947', 'gaussian_1987', 'flinn_1991', 'delong_2017', 'briere2_1999', 'boatman_2017', 'beta_2012', 'molnar_2013', 'modifiedgaussian_2006')
+  mod_names <- c('sharpeschoolhigh_1981', 'sharpeschoollow_1981', 'sharpeschoolfull_1981', 'johnsonlewin_1946', 'lactin2_1995', 'oneill_1972', 'quadratic_2008', 'ratkowsky_1983', 'rezende_2019', 'spain_1982', 'thomas_2012', 'thomas_2017', 'weibull_1995', 'kamykowski_1985', 'joehnk_2008', 'hinshelwood_1947', 'gaussian_1987', 'flinn_1991', 'delong_2017', 'briere2_1999', 'boatman_2017', 'beta_2012', 'modifiedgaussian_2006')
 
   if (model_name %in% mod_names == FALSE){
     stop("supplied model_name is not an available model in rTPC. Please check the spelling of model_name.")
