8500994603875d1c964612775e47af5ee1e04d63
Daniel Padfield <d.padfieldscfc@gmail.com>
2020-06-05 18:17:04 +0100

    add test for beta_2012

--- tests/testthat/test-beta_2012.R
+++ tests/testthat/test-beta_2012.R
@@ -1,26 +1,27 @@
-context("test-oneill_1972.R")
+context("test-beta_2012.R")
 
 # method: fit model and get predictions. Check these against others.
 
 # load in ggplot
 library(ggplot2)
 
+# subset for the first TPC curve
 data('chlorella_tpc')
 d <- subset(chlorella_tpc, curve_id == 1)
 
 # get start values and fit model
-start_vals <- get_start_vals(d$temp, d$rate, model_name = 'oneill_1972')
+start_vals <- get_start_vals(d$temp, d$rate, model_name = 'beta_2012')
 
 # fit model
-mod <- suppressWarnings(nls.multstart::nls_multstart(rate~oneill_1972(temp = temp, rmax, ctmax, topt, q10),
+mod <- nls.multstart::nls_multstart(rate~beta_2012(temp = temp, a, b, c, d, e),
                                     data = d,
-                                    iter = c(5,5,5,5),
+                                    iter = c(7,7,7,7,7),
                                     start_lower = start_vals - 10,
                                     start_upper = start_vals + 10,
-                                    lower = get_lower_lims(d$temp, d$rate, model_name = 'oneill_1972'),
-                                    upper = get_upper_lims(d$temp, d$rate, model_name = 'oneill_1972'),
+                                    lower = get_lower_lims(d$temp, d$rate, model_name = 'beta_2012'),
+                                    upper = get_upper_lims(d$temp, d$rate, model_name = 'beta_2012'),
                                     supp_errors = 'Y',
-                                    convergence_count = FALSE))
+                                    convergence_count = FALSE)
 
 # get predictions
 preds <- broom::augment(mod)
@@ -32,8 +33,8 @@ ggplot(preds) +
   theme_bw()
 
 # run test for oneill function
-testthat::test_that("oneill function works", {
+testthat::test_that("beta function works", {
   testthat::expect_equal(
     round(preds$.fitted, 1),
-    c(0.1, 0.2, 0.3, 0.5, 0.7, 1.0, 1.3, 1.6, 1.5, 1.1, 0.4, 0.0))
+    c(0.1, 0.2, 0.4, 0.6, 0.9, 1.1, 1.3, 1.4, 1.3, 1.1, 0.7, 0.0))
 })
