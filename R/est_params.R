a55aef413bad166cd9662ec9af53047eedd9912e
Daniel Padfield <d.padfieldscfc@gmail.com>
2020-06-12 09:57:15 +0100

    Suppress warnings in est_params

--- R/est_params.R
+++ R/est_params.R
@@ -18,16 +18,16 @@
 #' @export est_params
 
 est_params <- function(model){
-  t <- data.frame(rmax = tryCatch(rTPC::get_rmax(model), error = function(err) NA),
-                  topt = tryCatch(rTPC::get_topt(model), error = function(err) NA),
-                  ctmin = tryCatch(rTPC::get_ctmin(model), error = function(err) NA),
-                  ctmax = tryCatch(rTPC::get_ctmax(model), error = function(err) NA),
-                  e = tryCatch(rTPC::get_e(model), error = function(err) NA),
-                  eh = tryCatch(rTPC::get_eh(model), error = function(err) NA),
-                  q10 = tryCatch(rTPC::get_q10(model), error = function(err) NA),
-                  thermal_safety_margin = tryCatch(rTPC::get_thermalsafetymargin(model), error = function(err) NA),
-                  thermal_tolerance = tryCatch(rTPC::get_thermaltolerance(model), error = function(err) NA),
-                  skewness = tryCatch(rTPC::get_skewness(model), error = function(err) NA),
+  t <- data.frame(rmax = suppressWarnings(tryCatch(rTPC::get_rmax(model), error = function(err) NA)),
+                  topt = suppressWarnings(tryCatch(rTPC::get_topt(model), error = function(err) NA)),
+                  ctmin = suppressWarnings(tryCatch(rTPC::get_ctmin(model), error = function(err) NA)),
+                  ctmax = suppressWarnings(tryCatch(rTPC::get_ctmax(model), error = function(err) NA)),
+                  e = suppressWarnings(tryCatch(rTPC::get_e(model), error = function(err) NA)),
+                  eh = suppressWarnings(tryCatch(rTPC::get_eh(model), error = function(err) NA)),
+                  q10 = suppressWarnings(tryCatch(rTPC::get_q10(model), error = function(err) NA)),
+                  thermal_safety_margin = suppressWarnings(tryCatch(rTPC::get_thermalsafetymargin(model), error = function(err) NA)),
+                  thermal_tolerance = suppressWarnings(tryCatch(rTPC::get_thermaltolerance(model), error = function(err) NA)),
+                  skewness = suppressWarnings(tryCatch(rTPC::get_skewness(model), error = function(err) NA)),
                   stringsAsFactors = FALSE)
 
   return(t)
