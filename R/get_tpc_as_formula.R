7f6114ed68eecaaf19999d7a2f000e2b95e1d930
Francis Windram <francis.windram17@imperial.ac.uk>
2025-07-25 03:09:01 +0100

    Add functions for quick default TPC fitting (fixes #77)

---
+++ R/get_tpc_as_formula.R
@@ -0,0 +1,40 @@
+#' Get a formula object for calling a TPC
+#'
+#' @param model_name the name of the model being fitted
+#' @param temp the name of the temperature column
+#' @param trait the name of the trait column
+#' 
+#' @author Francis Windram
+#' @return A formula calling the expected TPC
+#' @concept helper
+#' 
+#' @examples
+#' get_tpc_as_formula("briere1_1999", "temperature", "rate")
+#' # > rate ~ briere1_1999(temp = temperature, tmin, tmax, a)
+#' 
+#' @export get_tpc_as_formula
+
+get_tpc_as_formula <- function(model_name, temp, trait){
+  
+  if (length(temp) != 1) {
+    cli::cli_abort(c("x"="Supplied {.arg temp} is not a column name (contains {.val {length(temp)}} element{?s})"))
+  }
+  
+  if (length(trait) != 1) {
+    cli::cli_abort(c("x"="Supplied {.arg trait} is not a column name (contains {.val {length(trait)}} element{?s})"))
+  }
+  
+  mod_names <- get_model_names(returnall = TRUE)
+  model_name <- tryCatch(rlang::arg_match(model_name, mod_names), error = function(e){
+    cli::cli_abort(c("x"="Supplied {.arg model_name} ({.val {model_name}}) is not an available model in rTPC.",
+                     "!"="Please check the spelling of {.arg model_name}.",
+                     " "="(run {.fn rTPC::get_model_names} to see all valid names.)",
+                     ""), call=rlang::caller_env(n=4))
+  })
+  
+  tpcfunc <- get(model_name)
+  tpcargs <- names(formals(tpcfunc))
+  
+  formulastr <- glue::glue("{trait}~{model_name}(temp = {temp}, {paste(tpcargs[tpcargs != 'temp'], collapse = ', ')})")
+  return(as.formula(formulastr))
+}
---
+++ R/quickfit_tpc.R
@@ -0,0 +1,57 @@
+#' Perform a quick tpc fit
+#'
+#' @description Performs a simple TPC fit using \code{\link[nls.multstart]{nls_multstart}}. This function tries to use a sensible default configuration, 
+#' however if you need to use the more esoteric elements of \code{\link[nls.multstart]{nls_multstart}} then you will need to construct your own.
+#' @param data the data to fit a model to
+#' @param model_name the model name as a string
+#' @param temp the column name (as a string) containing the temperature data
+#' @param trait the column name (as a string) containing the temperature data
+#' @param start_adjusts any additive adjustments to make to the lower and upper starting bounds
+#' @param iter number of combinations of starting parameters which will be tried (as in \code{\link[nls.multstart]{nls_multstart}})
+#' @param lhstype method to use for Latin Hypercube Sampling using \code{\link[lhs]{lhs}} (as in \code{\link[nls.multstart]{nls_multstart}})
+#' @param gridstart whether to run a gridstart approach (interpreting iter as the number of samples to take across each parameter, 
+#' so \code{3} will become \code{c(3,3,3)} for a 3-parameter model)
+#' @param force whether to force a gridstart even with very large numbers of iterations
+#' @author Francis Windram
+#' @return Named list of lower limits given the data and model being fitted
+#' @concept helper
+#' 
+#' @examples
+#' subs <- subset(chlorella_tpc, curve_id == 1)
+#' quickfit_tpc(subs, "briere1_1999", "temp", "rate")
+#' 
+#' quickfit_tpc(subs, "briere1_1999", "temp", "rate", start_adjusts = 10, iter = 150, lhstype = "maximin")
+#' 
+#' quickfit_tpc(subs, "briere1_1999", "temp", "rate", start_adjusts = 10, iter = 5, gridstart=TRUE)
+#' 
+#' @export quickfit_tpc
+
+
+quickfit_tpc <- function(data, model_name, temp, trait, start_adjusts = 0, iter = 150, lhstype = "none", gridstart=FALSE, force=FALSE) {
+  
+  rlang::check_installed("nls.multstart")
+  
+  start_vals <- get_start_vals(data[,temp], data[,trait], model_name = model_name)
+  
+  if (gridstart){
+    iter <- rep(iter, length(start_vals))
+    total_iterations <- prod(iter)
+    if (total_iterations > 1500 && !force) {
+      cli::cli_abort(c(
+        "x"="Massive gridstart detected! ({.val {total_iterations}} iterations)",
+        "!"="If this is intended, set force={.val {TRUE}}, else set a lower {.arg iter} value such as {.val {3}}."))
+    }
+  }
+  
+  mod <- nls.multstart::nls_multstart(get_tpc_as_formula(model_name, temp, trait),
+                       data = data,
+                       iter = iter,
+                       start_lower = start_vals - start_adjusts,
+                       start_upper = start_vals + start_adjusts,
+                       lower = get_lower_lims(data[, temp], data[, trait], model_name = model_name),
+                       upper = get_upper_lims(data[, temp], data[, trait], model_name = model_name),
+                       supp_errors = 'Y',
+                       lhstype = lhstype,
+                       convergence_count = FALSE)
+  return(mod)
+}
