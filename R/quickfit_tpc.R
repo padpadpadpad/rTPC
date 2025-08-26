597aab01dc78fc1c55566d98c2cc563c6fdc10a2
padpadpadpad <d.padfieldscfc@gmail.com>
2025-08-26 18:27:48 +0100

    Update quickfit functions

--- R/quickfit_tpc.R
+++ R/quickfit_tpc.R
@@ -1,83 +1,102 @@
 #' Perform a quick, automated, TPC fit
 #'
-#' @description Performs a simple TPC fit using \code{\link[nls.multstart]{nls_multstart}}. This function tries to use a sensible default configuration, 
+#' @description Performs a simple TPC fit using \code{\link[nls.multstart]{nls_multstart}}. This function tries to use a sensible default configuration,
 #' however if you want to use the more custom elements of \code{\link[nls.multstart]{nls_multstart}} then you will need to construct your own.
 #' @param data the data to fit a model to
 #' @param model_name the model name as a string
 #' @param temp the column name (as a string) containing the temperature data
 #' @param trait the column name (as a string) containing the temperature data
 #' @param start_adjusts any adjustments to make to the lower and upper starting bounds. If \code{0 < start_adjusts < 1}, this will be interpreted as a proportion of the base starting values.
-#' @param iter number of combinations of starting parameters which will be tried (as in \code{\link[nls.multstart]{nls_multstart}})
-#' @param lhstype method to use for Latin Hypercube Sampling using \code{\link[lhs]{lhs}} (as in \code{\link[nls.multstart]{nls_multstart}})
-#' @param gridstart whether to run a gridstart approach (interpreting iter as the number of samples to take across each parameter, 
-#' so \code{3} will become \code{c(3,3,3)} for a 3-parameter model)
-#' @param force whether to force a gridstart even with very large numbers of iterations
+#' @param iter number of combinations of starting parameters which will be tried . If a single value is provided, then a shotgun/random-search/lhs approach will be used to sample starting parameters from a uniform distribution within the starting parameter bounds. If a vector of the same length as the number of parameters is provided, then a gridstart approach will be used to define each combination of that number of equally spaced intervals across each of the starting parameter bounds respectively. Thus, c(5,5,5) for three fitted parameters yields 125 model fits. Supplying a vector for iter will override convergence_count.
+#' @param ... additional arguments to pass to \code{\link[nls.multstart]{nls_multstart}}.
 #' @author Francis Windram
 #' @return The nls model object of the fit model
 #' @concept helper
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' data("chlorella_tpc")
+#'
 #' d <- subset(chlorella_tpc, curve_id == 1)
-#' quickfit_tpc(d, "briere1_1999", "temp", "rate")
-#' 
+#'
 #' quickfit_tpc(
-#'   subs,
+#'   d,
 #'   "briere1_1999",
 #'   "temp",
 #'   "rate",
 #'   start_adjusts = 10,
-#'   iter = 150,
-#'   lhstype = "maximin"
+#'   iter = 150
 #' )
-#' 
+#'
 #' quickfit_tpc(
-#'   subs,
+#'   d,
 #'   "briere1_1999",
 #'   "temp",
 #'   "rate",
-#'   start_adjusts = 10,
-#'   iter = 5,
-#'   gridstart = TRUE
+#'   start_adjusts = 0.8,
+#'   iter = c(5,5,5)
 #' )
 #' }
-#' 
+#'
 #' @export quickfit_tpc
 
-
-quickfit_tpc <- function(data, model_name, temp, trait, start_adjusts = 0, iter = 150, lhstype = "none", gridstart=FALSE, force=FALSE) {
-  
+quickfit_tpc <- function(
+  data,
+  model_name,
+  temp,
+  trait,
+  start_adjusts = 0,
+  iter,
+  ...
+) {
   rlang::check_installed("nls.multstart")
-  
+
+  args <- list(...)
+
+  # if not supplied, set default
+  if (is.null(args$supp_errors)) {
+    args$supp_errors <- "Y"
+  }
+
   # The form of column extraction used below only works properly on dfs.
   data <- as.data.frame(data)
-  
-  start_vals <- rTPC::get_start_vals(data[,temp], data[,trait], model_name = model_name)
-  
+
+  # compute needed values
+  start_vals <- rTPC::get_start_vals(
+    data[, temp],
+    data[, trait],
+    model_name = model_name
+  )
+  lower = rTPC::get_lower_lims(
+    data[, temp],
+    data[, trait],
+    model_name = model_name
+  )
+  upper = rTPC::get_upper_lims(
+    data[, temp],
+    data[, trait],
+    model_name = model_name
+  )
+
   if (0 < start_adjusts && start_adjusts < 1) {
     start_adjusts <- start_vals * start_adjusts
   }
-  
-  if (gridstart){
-    iter <- rep(iter, length(start_vals))
-    total_iterations <- prod(iter)
-    if (total_iterations > 1500 && !force) {
-      cli::cli_abort(c(
-        "x"="Massive gridstart detected! ({.val {total_iterations}} iterations)",
-        "!"="If this is intended, set force={.val {TRUE}}, else set a lower {.arg iter} value such as {.val {3}}."))
-    }
-  }
-  
-  mod <- nls.multstart::nls_multstart(rTPC::get_tpc_as_formula(model_name, temp, trait),
-                       data = data,
-                       iter = iter,
-                       start_lower = start_vals - start_adjusts,
-                       start_upper = start_vals + start_adjusts,
-                       lower = rTPC::get_lower_lims(data[, temp], data[, trait], model_name = model_name),
-                       upper = rTPC::get_upper_lims(data[, temp], data[, trait], model_name = model_name),
-                       supp_errors = 'Y',
-                       lhstype = lhstype,
-                       convergence_count = FALSE)
+
+  mod <- do.call(
+    nls.multstart::nls_multstart,
+    c(
+      list(
+        formula = rTPC::get_tpc_as_formula(model_name, temp, trait),
+        data = data,
+        iter = iter,
+        start_lower = start_vals - start_adjusts,
+        start_upper = start_vals + start_adjusts,
+        lower = lower,
+        upper = upper
+      ),
+      args
+    )
+  )
+
   return(mod)
 }
--- R/quickfit_tpc_multi.R
+++ R/quickfit_tpc_multi.R
@@ -1,117 +1,87 @@
-#' Perform a parallelised quick tpc fit across models and curves
+#' Perform an automated quick tpc fit across models and curves
 #'
-#' @description Performs a parallelised TPC fit using \code{\link[nls.multstart]{nls_multstart}} and \code{\link[purrr]{map}}. This function tries to use a sensible default configuration, 
-#' however if you need to use the more esoteric elements of \code{\link[nls.multstart]{nls_multstart}} then you may need to construct your own running script.
+#' @description Performs a TPC fit using \code{\link[nls.multstart]{nls_multstart}} and \code{\link[purrr]{map}}. This function tries to use a sensible default configuration,
+#' however if you need to to use the more custom elements of \code{\link[nls.multstart]{nls_multstart}} then you will need to construct your own.
 #' @param d the data to fit a model to
 #' @param model_names a vector of model names to fit as strings
 #' @param temp the column name (as a string) containing the temperature data
 #' @param trait the column name (as a string) containing the temperature data
-#' @param start_adjusts any adjustments to make to the lower and upper starting bounds. If \code{0 < start_adjusts < 1}, this will be interpreted as a proportion of the base starting values.
-#' @param iter number of combinations of starting parameters which will be tried (as in \code{\link[nls.multstart]{nls_multstart}})
-#' @param lhstype method to use for Latin Hypercube Sampling using \code{\link[lhs]{lhs}} (as in \code{\link[nls.multstart]{nls_multstart}})
-#' @param gridstart whether to run a gridstart approach (interpreting iter as the number of samples to take across each parameter, 
-#' so \code{3} will become \code{c(3,3,3)} for a 3-parameter model)
-#' @param force whether to force a gridstart even with very large numbers of iterations
-#' 
+#' @param start_adjusts any adjustments to make to the lower and upper starting bounds. If \code{0 < start_adjusts < 1}, this will be interpreted as a proportion of the base starting values. If above 1, it will add and substract that value from the base starting values.
+#' @param iter number of combinations of starting parameters which will be tried . If a single value is provided, then a shotgun/random-search/lhs approach will be used to sample starting parameters from a uniform distribution within the starting parameter bounds. If a vector of the same length as the number of parameters is provided, then a gridstart approach will be used to define each combination of that number of equally spaced intervals across each of the starting parameter bounds respectively. Thus, c(5,5,5) for three fitted parameters yields 125 model fits. Supplying a vector for iter will override convergence_count.
+#' @param ... additional arguments to pass to \code{\link[nls.multstart]{nls_multstart}}.
 #' @note The parameters \code{temp}, \code{trait}, \code{start_adjusts}, \code{iter}, \code{lhstype}, \code{gridstart}, or \code{force} can be specified per-model
 #'  by providing a vector of values of a length equal to the number of models to be fit.
-#' 
+#'
 #' @author Francis Windram
+#' @author Daniel Padfield
 #' @return A tibble of model fits
 #' @concept helper
-#' 
+#'
 #' @examples
 #' \dontrun{
+#' # load example data
 #' data("chlorella_tpc")
-#' d2 <- subset(chlorella_tpc, curve_id <= 60)
-#' 
-#' # Set up daemons for parallelism
-#' mirai::daemons(2)
-#' 
-#' quickfit_tpc_multi(d2, c("briere1_1999", "briere2_1999"), "temp", "rate")
-#' 
-#' quickfit_tpc_multi(d2, c("briere1_1999", "briere2_1999"), "temp", "rate", start_adjusts = 10)
-#' 
-#' quickfit_tpc_multi(d2, c("briere1_1999", "briere2_1999"), "temp", "rate", iter = c(100, 150))
-#' 
-#' mirai::daemons(0)
-#' }
+#'
+#' # subset for just the first 5 curves
+#' d <- subset(chlorella_tpc, curve_id <= 5)
+#'
+#'
+#' quickfit_tpc_multi(d, c("briere1_1999", "briere2_1999"), "temp", "rate")
+#'
+#' quickfit_tpc_multi(d, c("briere1_1999", "briere2_1999"), "temp", "rate", start_adjusts = 10)
+#'
+#' quickfit_tpc_multi(d, c("briere1_1999", "briere2_1999"), "temp", "rate", iter = c(100, 150))
+#'}
 #' @export quickfit_tpc_multi
 
-quickfit_tpc_multi <- function(d, model_names, temp, trait, start_adjusts = 0, iter = 150, lhstype = "none", gridstart=FALSE, force=FALSE) {
-  
-  rlang::check_installed(c("purrr", "mirai", "dplyr", "tidyr"), version = c("1.1.0", NA, NA, NA))
-  
-  if (!mirai::daemons_set()) {
-    cli::cli_abort(c(
-      "x" = "To perform parallel fitting, you must initialise mirai daemons.",
-      ">" = "Please run {.fn mirai::daemons} with the number of cores you wish to use.",
-      ""))
-  }
-  
-  rlang::check_installed(c("nls.multstart", "mirai", "carrier"))
-  
-  # Check model_names to make sure models are in the list (rather than doing this on each process)
-  mod_names <- rTPC::get_model_names(returnall = TRUE)
-  to_fit <- intersect(model_names, mod_names)
-  unknown <- setdiff(model_names, mod_names)
-  
-  if (length(to_fit) < 1) {
-    cli::cli_abort(c("x" = "No valid models found! Got: {.val {model_names}}",
-                     "!"="Please check the spellings within {.arg model_names}.",
-                     " "="(run {.fn rTPC::get_model_names} to see all valid names.)",
-                     ""))
-  } else if (length(unknown) > 0) {
-    cli::cli_warn(c("!" = "Unknown models specified. Ignoring {.val {unknown}}"))
-  }
-  
-  nesting_cols <- c(temp, trait)
-  
-  fit_models <- d |> 
-    tidyr::nest(data = dplyr::all_of(nesting_cols)) |> 
-    dplyr::mutate(
-      mods = purrr::map(
-        data,
-        purrr::in_parallel(
-          \(x) .quickfit_multi(x, 
-                               model_names = to_fit,
-                               temp = temp,
-                               trait = trait,
-                               start_adjusts = start_adjusts,
-                               iter = iter,
-                               lhstype = lhstype,
-                               gridstart = gridstart,
-                               force = force
-          ),
-          .quickfit_multi = .quickfit_multi,
-          to_fit = to_fit,
-          temp = temp,
-          trait = trait,
-          start_adjusts = start_adjusts,
-          iter = iter,
-          lhstype = lhstype,
-          gridstart = gridstart,
-          force = force
-        ),
-        .progress = TRUE
-      )
-    ) |>
-    tidyr::unnest(mods)
-  
-  return(fit_models)
-  
-}
+quickfit_tpc_multi <- function(
+  data,
+  model_names,
+  temp,
+  trait,
+  start_adjusts = 0,
+  iter,
+  ...
+) {
+  # capture additional args
+  args <- list(...)
 
-.quickfit_multi <- function(data, model_names, temp, trait, start_adjusts=0, iter=150, lhstype="none", gridstart=FALSE, force=FALSE) {
+  # if not supplied, set default
+  if (is.null(args$supp_errors)) {
+    args$supp_errors <- "Y"
+  }
+
+  # check lengths of all elements of args
+  extra_args_lengths <- sapply(args, length)
+  extra_args_bad <- names(extra_args_lengths[
+    extra_args_lengths != 1 & extra_args_lengths != length(model_names)
+  ])
+
+  if (
+    any(extra_args_lengths != 1 & extra_args_lengths != length(model_names))
+  ) {
+    cli::cli_abort(c(
+      "x" = "Some of the arguments have the wrong number of values.",
+      "!" = "Please check the number of values for {.val {extra_args_bad}}. They need to be either of length one or the length equal to the number of models being fit ({length(model_names)})."
+    ))
+  }
+
   # Coerce args (basically make df of params)
-  df <- tibble::tibble(model=model_names, temp, trait, start_adjusts, iter, lhstype , gridstart, force)
-  
+  df <- tibble::tibble(
+    model = model_names,
+    temp,
+    trait,
+    start_adjusts,
+    iter,
+    !!!args
+  )
+
   # Map tpcs using quickfit
   out <- purrr::pmap(df, \(...) list(rTPC::quickfit_tpc(data, ...)))
-  
+
   # Get some lovely names
   names(out) <- model_names
-  
+
   # Cast to tibble and return
-  return(tibble::as.tibble(out))
+  return(tibble::as_tibble(out))
 }
